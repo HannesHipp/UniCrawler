a674134c97fe7b742cfa175c5f20b234ad00145d
Hannes Hipp <hhipp98@gmail.com>
2024-09-15 04:13:43 +0200

    added on quit event and error handling

--- Framework/App.py
+++ Framework/App.py
@@ -9,29 +9,40 @@ class App(QWidget):
 
     def __init__(self):
         os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
-        self.qApp = QApplication(sys.argv)
+        self.q_app = QApplication(sys.argv)
         super().__init__()
-        self.stackedWidget = QStackedWidget()
-        mainLayout = QVBoxLayout()
-        mainLayout.setSpacing(0)
-        mainLayout.setContentsMargins(0, 0, 0, 0)
-        mainLayout.addWidget(self.stackedWidget)
+        self.stacked_widget = QStackedWidget()
+        main_layout = QVBoxLayout()
+        main_layout.setSpacing(0)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.addWidget(self.stacked_widget)
         self.setFixedSize(800, 500)
-        self.setLayout(mainLayout)
+        self.setLayout(main_layout)
         App.instance = self
         self.frames = []
+        self.current_frame = None
+        self.q_app.aboutToQuit.connect(self.on_exit)
         self.show()
 
     def start_with(self, frame):
         frame.show()
-        sys.exit(self.qApp.exec_())
+        try:
+            sys.exit(self.q_app.exec_())
+        except Exception as e:
+            print(e)
+            self.current_frame.save_datapoints()
+            sys.exit(1)
 
     def add_frame(self, frame):
         self.frames.append(frame)
-        self.stackedWidget.addWidget(frame)
+        self.stacked_widget.addWidget(frame)
 
-    def selectFrame(self, frame):
+    def select_frame(self, frame):
         if not frame in self.frames:
             self.add_frame(frame)
         index = self.frames.index(frame)
-        self.stackedWidget.setCurrentIndex(index)
+        self.stacked_widget.setCurrentIndex(index)
+        self.current_frame = frame
+
+    def on_exit(self):
+        self.current_frame.save_datapoints()
